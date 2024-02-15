f26ad65fd1a7c918c9ff15620a9a5d7d2aaac86c
Rob Gayle <bob01@rogers.com>
2024-02-14 23:11:20 -0500

    YGE ESC - initial

---
+++ RF2YGE/pages.lua
@@ -0,0 +1,7 @@
+local PageFiles = {}
+
+-- ESC pages.
+PageFiles[#PageFiles + 1] = { title = "ESC Setup", script = "esc_setup.lua" }
+PageFiles[#PageFiles + 1] = { title = "ESC Debug", script = "esc_debug.lua" }
+
+return PageFiles
